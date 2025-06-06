92e6ecc508b329a37951ebf2aa3cea5e023ab4f3
LilithNML <lilithmorningstar468@gmail.com>
2025-06-05 20:01:23 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
+let planes = [];
 
+fetch("assets/data/planes.json")
+  .then(res => res.json())
+  .then(data => {
+    planes = data.planes;
+  });
+
+document.getElementById("generar").addEventListener("click", () => {
+  if (planes.length === 0) return;
+
+  const index = Math.floor(Math.random() * planes.length);
+  const planElegido = planes.splice(index, 1)[0]; // elimina el plan para no repetir
+
+  document.getElementById("plan").textContent = planElegido;
+
+  if (planes.length === 0) {
+    document.getElementById("generar").disabled = true;
+    document.getElementById("generar").textContent = "¡Ya viste todos!";
+  }
+});
